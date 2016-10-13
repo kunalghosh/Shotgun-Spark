46217a669bb4d01b02fc628f561ecd6526bef328
Kunal Ghosh <kunal.t2@gmail.com>
2016-10-13 21:35:00 +0300

    Shotgun implementation with twice the number of dimensions. Normvalue diverges, should converge. Had a similar problem when initially implementing in matlab. Use the 1_d implementation instead of this one.

--- src/python/RDD_impl.py
+++ src/python/RDD_impl.py
@@ -26,6 +26,7 @@ def F(x,A,y,lamda):
     LASSO function
     """
     return 0.5 * np.square(np.linalg.norm(np.dot(A,x) - y)) + lamda*np.linalg.norm(x,1)
+    # return 0.5 * np.square(np.linalg.norm(np.dot(np.concatenate((A,-A),axis=1),x) - y)) + lamda*np.sum(x)
 
 if __name__ == "__main__":
     data = sio.loadmat("../../data/Mug32_singlepixcam.mat",mat_dtype=True)
@@ -73,10 +74,10 @@ if __name__ == "__main__":
     ## aTa = sc.parallelize(xrange(d)).map(lambda cid: np.dot(np.transpose(A[:,cid]),A))
 
     # Parallelizing column wise
-    data = sc.parallelize(xrange(2*d)).\
+    data = sc.parallelize(xrange(d)).\
             map(lambda cid: (cid,
-                            np.dot(np.transpose(A[:,cid % d]),A),
-                            np.dot(np.transpose(y),A[:,cid % d]))).\
+                            np.dot(np.transpose(A[:,cid]),A),
+                            np.dot(np.transpose(y),A[:,cid]))).\
             persist() #here cid is the column index.
     # NOTE: When computing AtA if A doesn't fit into memory we:
     # 1. Compute individual elements of the AtA matrix.
@@ -99,30 +100,54 @@ if __name__ == "__main__":
         
 
     for i in range(10):
-        P_vals=[1,2,4,6,8,10,20,30,40,50,60,70,80,90,100,110]
+        P_vals=[3,1,2,4,6,8,10,20,30,40,50,60,70,80,90,100,110]
         for P in P_vals:
             for iter in xrange(maxiters):
                 randIdxs = np.random.permutation(2*d)
                 randPidxs = randIdxs[0:P]
-
+                # print(randPidxs[randPidxs <= d])
+                # print(randPidxs[randPidxs > d])
+                # print(randPidxs) 
                 # Get the data corresponding to the P indexes.
                 # NOTE: Problem !! cid(s) in data_sub are always
                 #       less than d. So the map with deltaF will
                 #       not work. !
-                data_sub = data.filter(lambda (cid, AtAi, ytAi): cid in randPidxs or cid+d in randPidxs)
+                # data_sub = data.filter(lambda (cid, AtAi, ytAi): cid in randPidxs or cid+d in randPidxs)
                 # updates = data_sub.map(lambda (cid, AtAi, ytAi): (cid, np.dot(x.transpose(), AtAi) - ytAi + lamda.value)).collect()
-                updates = np.array(data_sub.map(deltaF).collect())
+                
+                # We do the updates in two parts
+                # for column ids in randPidxs less than d
+                data_sub_lt_d = data.filter(lambda (cid, AtAi, ytAi): cid in randPidxs)
+                updates_lt_d = data_sub_lt_d.map(deltaF).collect()
+
+                # for column ids in randPidxs greater than d
+                data_sub_gt_d = data.filter(lambda (cid, AtAi, ytAi): cid+d in randPidxs)\
+                                    .map(lambda (cid, AtAi, ytAi): (cid+d, AtAi, ytAi)) # update the column ids
+                updates_gt_d = data_sub_gt_d.map(deltaF).collect()
+
+                # Sanity check for P = 1 one of the data_sub_--_d should be empty
+                # Count of Columns (with cid < d and cid > d) must add up to P. Since cid(s) come from randPidxs
+                assert P == data_sub_lt_d.count() + data_sub_gt_d.count() , "(# of cid(s) < d) {} + {} (# of cid(s) > d) != {} (= P)".format(data_sub_lt_d.count(),data_sub_gt_d.count(),P)
+                assert P == len(updates_lt_d) + len(updates_gt_d) , "(# of updates from cid(s) < d) {} + {} (# of updates from cid(s) > d)!= {} (= P)".format(len(updates_lt_d),len(updates_gt_d),P)
+
+                # print(updates_lt_d)
+                # print(updates_gt_d)
+                
+                updates = np.array(updates_lt_d+updates_gt_d)
                 idxs = np.array(updates[:,0],int)
-                # x[idxs] += np.maximum(-1*x[idxs], -1*updates[:,1]/beta.value)
+                # print(idxs)
+                # print(np.max(np.array([-1*x[idxs], -1*updates[:,1]/beta.value]),axis=0))
                 x[idxs] += np.max(np.array([-1*x[idxs], -1*updates[:,1]/beta.value]),axis=0)
 
                 if iter % 100 == 0:
                     # print(idxs)
                     # print(x[x!=0])
                     x_1d = x[d:] - x[:d]
-                    # print(x_1d[x_1d!=0])
+                    print(x_1d[x_1d!=0])
                     # # Following line would change if A doesn't fit in memory
+                    # print("Nonzero x {}".format(np.nonzero(x_1d))) 
                     print("Iter {} NormVal {}".format(iter, F(np.reshape(x_1d,(d,1)), A, y, lamda.value))) 
+                    # print("Iter {} NormVal {}".format(iter, F(np.reshape(x,(2*d,1)), A, y, lamda.value))) 
     # # Normalizing A
     # data = data.map(lambda (cid,a,y):\
     #         (cid,a/np.sqrt(np.sum(np.square(a))),y))
